545f07b8ae5a25e8ff6854ec8fdfa399da17a995
Emilson Silva <emilsongyn17@gmail.com>
2026-02-06 10:16:24 -0300

    feat: add color palette tokens

---
+++ lib/design_system/tokens/color_tokens.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DSColorPalette {
+  static const Color moreInk = Color(0xFF111111);
+  static const Color moreSand = Color(0xFFF2E8D5);
+  static const Color moreCoral = Color(0xFFEA6D5A);
+  static const Color moreSky = Color(0xFF2E86AB);
+  static const Color moreSlate = Color(0xFF5C677D);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+
+  static const Color surface = Color(0xFFF8F8F6);
+  static const Color border = Color(0xFFE0E0E0);
+  static const Color disabled = Color(0xFFBDBDBD);
+
+  static const Color success = Color(0xFF2E7D32);
+  static const Color warning = Color(0xFFED6C02);
+  static const Color error = Color(0xFFD32F2F);
+  static const Color info = Color(0xFF0288D1);
+}
